1ac11e5e9bbf0d5b198bf1953092236aa2de20c9
FernandoSBorges <fernandodasilvaborges@gmail.com>
2024-06-19 14:30:00 -0300

    human_L23

--- test/L23Net/cellwrapper.py
+++ test/L23Net/cellwrapper.py
@@ -1,12 +1,9 @@
-import os
 import sys
+import os
 
-# cellName = 'HL23PYR'
+def loadCell_HL23PYR(cellName):
 
-    
-def loadCell_Net(cellName):
-
-    templatepath = 'models/NeuronTemplate.hoc'
+    templatepath = 'models/NeuronTemplate_HL23PYR.hoc'
     biophysics = 'models/biophys_' + cellName + '.hoc'
     morphpath = 'morphologies/' + cellName + '.swc'
 
@@ -22,9 +19,91 @@ def loadCell_Net(cellName):
     except:
         pass
 
-    cell = getattr(h, 'NeuronTemplate')(morphpath)
+    cell = getattr(h, 'NeuronTemplate_HL23PYR')(morphpath)
     
     print (cell)
 
+    h.biophys_HL23PYR(cell)
+
     return cell
+
+
+def loadCell_HL23VIP(cellName):
+
+    templatepath = 'models/NeuronTemplate_HL23VIP.hoc'
+    biophysics = 'models/biophys_' + cellName + '.hoc'
+    morphpath = 'morphologies/' + cellName + '.swc'
+
+    from neuron import h
+
+    h.load_file("stdrun.hoc")
+    h.load_file('import3d.hoc')
+
+    h.xopen(biophysics)
+        
+    try:
+       h.xopen(templatepath)
+    except:
+        pass
+
+    cell = getattr(h, 'NeuronTemplate_HL23VIP')(morphpath)
     
+    print (cell)
+
+    h.biophys_HL23VIP(cell)
+
+    return cell
+
+
+def loadCell_HL23PV(cellName):
+
+    templatepath = 'models/NeuronTemplate_HL23PV.hoc'
+    biophysics = 'models/biophys_' + cellName + '.hoc'
+    morphpath = 'morphologies/' + cellName + '.swc'
+
+    from neuron import h
+
+    h.load_file("stdrun.hoc")
+    h.load_file('import3d.hoc')
+
+    h.xopen(biophysics)
+        
+    try:
+       h.xopen(templatepath)
+    except:
+        pass
+
+    cell = getattr(h, 'NeuronTemplate_HL23PV')(morphpath)
+    
+    print (cell)
+
+    h.biophys_HL23PV(cell)
+
+    return cell
+
+
+def loadCell_HL23SST(cellName):
+
+    templatepath = 'models/NeuronTemplate_HL23SST.hoc'
+    biophysics = 'models/biophys_' + cellName + '.hoc'
+    morphpath = 'morphologies/' + cellName + '.swc'
+
+    from neuron import h
+
+    h.load_file("stdrun.hoc")
+    h.load_file('import3d.hoc')
+
+    h.xopen(biophysics)
+        
+    try:
+       h.xopen(templatepath)
+    except:
+        pass
+
+    cell = getattr(h, 'NeuronTemplate_HL23SST')(morphpath)
+    
+    print (cell)
+
+    h.biophys_HL23SST(cell)
+
+    return cell
